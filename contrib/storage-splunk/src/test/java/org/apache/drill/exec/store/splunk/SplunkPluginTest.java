a8103578fd4aeb14415e7ffe6b1205e29e26664a
Vitalii Diravka <vitalii@apache.org>
2021-07-26 15:10:34 +0300

    Disable flaky test

--- contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/SplunkPluginTest.java
+++ contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/SplunkPluginTest.java
@@ -166,6 +166,7 @@ public class SplunkPluginTest extends SplunkBaseTest {
   }
 
   @Test
+  @Ignore("the result is not consistent on system table")
   public void testExplicitFieldsWithSourceType() throws Exception {
     String sql = "SELECT action, _sourcetype, _subsecond, _time FROM splunk._audit WHERE sourcetype='audittrail' LIMIT 5";
     client.testBuilder()
